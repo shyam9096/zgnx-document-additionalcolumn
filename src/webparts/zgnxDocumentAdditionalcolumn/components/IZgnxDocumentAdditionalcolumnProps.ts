12182a5c67d597dc0412b5e69129320dd5aab94f
Shyam Thorat <thoratshyam0@gmail.com>
2022-03-30 23:36:47 -0700

    SharePoint Document library Webpart with additional column

---
+++ src/webparts/zgnxDocumentAdditionalcolumn/components/IZgnxDocumentAdditionalcolumnProps.ts
@@ -0,0 +1,10 @@
+export interface IZgnxDocumentAdditionalcolumnProps {
+  description: string;
+  context: any;
+  list: any;
+  multiSelect: string[];
+  ShowNameModified: boolean;
+  ShowSubFolders: boolean;
+  SiteUrl: any;
+  spweb: any;
+}
---
+++ src/webparts/zgnxDocumentAdditionalcolumn/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IZgnxDocumentAdditionalcolumnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ZgnxDocumentAdditionalcolumnWebPartStrings' {
+  const strings: IZgnxDocumentAdditionalcolumnWebPartStrings;
+  export = strings;
+}
